a7d6398909a7a56079bd52462ac7e36ad6a37321
Oscar Pairazaman <70730776+OscarEd08@users.noreply.github.com>
2023-02-27 12:23:35 -0500

    quitando la etiqueta sin usar <script>

--- index.php
+++ index.php
@@ -5,7 +5,6 @@
     <title>CRUD</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
-    <script>src="index.js"</script>
 </head>
 <body>
     <div class="container-fluid mt-2">
